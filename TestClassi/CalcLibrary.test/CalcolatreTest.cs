6b38961e888a263cd09d901443943b5ebca93cdd
Mattia <Mattia@DESKTOP-1DT4BRN>
2018-04-03 21:14:27 +0100

    fine

---
+++ TestClassi/CalcLibrary.test/CalcolatreTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalcLibrary;
+
+namespace CalcLibrary.test
+{
+    [TestClass]
+    public class CalcolatreTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] v1 = new int[] { 11, 22, 13, 4 };
+            int[] v2 = new int[] { 4,11,13,22 };
+            int[] vettoreOrdinato = Calcolatrice.Ordina(v1);
+            CollectionAssert.AreEqual(v2, vettoreOrdinato);
+
+        }
+    }
+}
---
+++ TestClassi/CalcLibrary.test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalcLibrary.test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalcLibrary.test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bf1f83f9-be48-4e59-a1c1-23289223d675")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
